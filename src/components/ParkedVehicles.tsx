fa097718a7827821fb5ef924352011442e810906
Ashutoshsingh Pardeshi <ashutoshsingh.pardeshi@pattern.com>
2023-07-12 16:36:13 +0530

    feat:adding the axios calls

--- src/components/ParkedVehicles.tsx
+++ src/components/ParkedVehicles.tsx
@@ -1,199 +1,92 @@
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
+import backgroundImage2 from "../assets/bg-copy.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { OwnerSchema } from "./OwnerInterface";
 
 const ParkedVehicles = () => {
-  const data = [
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-    {
-      id: 1,
-      image: "png",
-      licenseNumber: "ABC1234",
-      entryTime: "1:00 pm",
-      parkedLocation: "A9",
-    },
-  ];
+  const [users, setUsers] = useState<OwnerSchema[]>([]);
+  const [time, setTime] = useState(new Date().toLocaleTimeString("en-US"));
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/owners").then((res) => {
+      console.log(res.data);
+      setUsers(res.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString("en-US"));
+    });
+  });
   const navigate = useNavigate();
   return (
     <>
-      <Navbar />
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImage2})`,
+          backgroundSize: "cover",
+          height: "100vh",
+        }}
+      >
+        <Navbar />
 
-      <div className="container mt-3">
-        <table className="table">
-          <thead>
-            <tr style={{ backgroundColor: "red" }}>
-              <th className="bg-info-subtle" scope="col">
-                Entry Id
-              </th>
-              <th className="bg-info-subtle" scope="col">
-                Image
-              </th>
-              <th className="bg-info-subtle" scope="col">
-                License Number
-              </th>
-              <th className="bg-info-subtle" scope="col">
-                Entry Time
-              </th>
-              <th className="bg-info-subtle" scope="col">
-                Parked Location
-              </th>
-              <th className="bg-info-subtle" scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((vehicle) => (
-              <tr key={vehicle.id}>
-                <th scope="row">{vehicle.id}</th>
-                <td>{vehicle.image}</td>
-                <td>{vehicle.licenseNumber}</td>
-                <td>{vehicle.entryTime}</td>
-                <td>{vehicle.parkedLocation}</td>
-                <td>
-                  <button
-                    type="button"
-                    className="btn btn-info"
-                    onClick={() => {
-                      navigate("/parking-details");
-                    }}
-                  >
-                    View Details
-                  </button>
-                </td>
+        <div className="container mt-3">
+          <div className="badge bg-info bg-opacity-75">
+            <p className="display-6 px-2 pt-2">{time}</p>
+          </div>
+          <table className="table">
+            <thead>
+              <tr style={{ backgroundColor: "red" }}>
+                <th className="bg-info-subtle" scope="col">
+                  Entry Id
+                </th>
+                <th className="bg-info-subtle" scope="col">
+                  Image
+                </th>
+                <th className="bg-info-subtle" scope="col">
+                  License Number
+                </th>
+                <th className="bg-info-subtle" scope="col">
+                  Entry Time
+                </th>
+                <th className="bg-info-subtle" scope="col">
+                  Parked Location
+                </th>
+                <th className="bg-info-subtle" scope="col"></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users &&
+                users.map((vehicle) => (
+                  <tr key={vehicle._id + "12345"}>
+                    <th scope="row">{vehicle._id.substring(18)}</th>
+                    <td>vehicle.image</td>
+                    <td>{vehicle.vehicle.licenseNumber}</td>
+                    <td>
+                      {new Date(
+                        vehicle.parkingDetails.checkIn
+                      ).toLocaleTimeString("en-US")}
+                    </td>
+                    <td>{vehicle.parkingDetails.parkedSpot}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        onClick={() => {
+                          navigate(`/parking-details/${vehicle._id}`);
+                        }}
+                      >
+                        View Details
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
