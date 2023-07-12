ec196b700a7449e5b4e57d63d01f4e00812b837d
Ashutoshsingh Pardeshi <ashutoshsingh.pardeshi@pattern.com>
2023-07-12 16:34:51 +0530

    update:changing the routes functionality due to changes in schema

--- backend/routes/Owner.routes.js
+++ backend/routes/Owner.routes.js
@@ -3,37 +3,6 @@ const Owner = require("../models/Owner.model");
 const ParkingDetails = require("../models/ParkingDetails.model");
 const router = express.Router();
 
-const addVehicle = async ({ ...props }) => {
-  const { ownerId, licenseNumber, parkedSpot } = props;
-
-  const vehicle = await new ParkingDetails({
-    ownerId,
-    licenseNumber,
-    parkedSpot,
-    checkIn: new Date(),
-  });
-  vehicle.save().then((details) => {
-    console.log("Vehicle details added successfully");
-    console.log(details);
-  });
-  console.log("Vehicle: ", vehicle);
-  return vehicle;
-};
-
-const removeVehicle = ({ ...props }) => {
-  const { ownerId } = props;
-  try {
-    ParkingDetails.findOneAndRemove({
-      ownerId,
-    }).then((vehicle) => {
-      console.log("Vehicle details removed successfully");
-      console.log(vehicle);
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 /**
  * @route GET /api/owners
  * @desc Get all the owners from the db
@@ -102,19 +71,17 @@ router.post("", (req, res) => {
         color,
         seatingCapacity,
       },
+      parkingDetails: {
+        checkIn: new Date(),
+        parkedSpot,
+      },
     })
       .save()
       .then(async (owner) => {
         const ownerId = owner._id;
-        const vehicle = await addVehicle({
-          ownerId,
-          licenseNumber,
-          parkedSpot,
-        });
         res.status(201);
-        res.send({ owner, vehicle });
+        res.send(owner);
         console.log("Owner Details: ", owner);
-        console.log("Vehicle Details: ", vehicle);
       });
   } catch (e) {
     res.send(e);
@@ -127,54 +94,13 @@ router.post("", (req, res) => {
  */
 router.delete("/:id", (req, res) => {
   const ownerId = req.params.id;
-
   try {
     // Owner.findOneAndRemove({ _id: new ObjectId(owner_id) }).then((owner) => {
     Owner.findByIdAndRemove(ownerId).then((owner) => {
-      try {
-        ParkingDetails.findOneAndRemove({
-          ownerId,
-        }).then((vehicle) => {
-          console.log("Vehicle details removed successfully");
-          console.log(vehicle);
-          console.log(owner);
-          res.send({ owner, vehicle });
-        });
-      } catch (err) {
-        console.log(err);
-      }
+      res.status(200);
+      res.send(owner);
+      console.log(owner);
     });
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-/**
- * @route POST /api/owners/addVehicle/:ownerId
- * @desc Adds a new vehicle linked to the user
- */
-router.post("/addVehicle/:ownerId", async (req, res) => {
-  const { ownerId } = req.params;
-  const { licenseNumber, parkedSpot } = req.body;
-  try {
-    const vehicle = await addVehicle({ ownerId, licenseNumber, parkedSpot });
-    res.status(201);
-    res.send(vehicle);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-/**
- * @route DELETE /api/owners/removeVehicle/:ownerId
- * @desc Remove the vehicle linked to the user
- */
-router.delete("/removeVehicle/:ownerId", (req, res) => {
-  const { ownerId } = req.params;
-  try {
-    removeVehicle({ ownerId });
-    res.status(204);
-    res.send("Deleted Successfully");
   } catch (err) {
     console.log(err);
   }
