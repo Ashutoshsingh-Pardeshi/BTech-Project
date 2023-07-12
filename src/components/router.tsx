8d4c3370cb5eb5c9e06b2e7835d212243c345d40
Ashutoshsingh Pardeshi <ashutoshsingh.pardeshi@pattern.com>
2023-07-13 01:08:13 +0530

    feat:added the form componenet

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -3,12 +3,14 @@ import Home from "./Home";
 import ParkedVehicles from "./ParkedVehicles";
 import ViewDetails from "./ViewDetails";
 import PaymentDetails from "./PaymentDetails";
+import AddVehicle from "./AddVehicle";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/parked-vehicles", element: <ParkedVehicles /> },
   { path: "/parking-details/:id", element: <ViewDetails /> },
   { path: "/check-out/:id", element: <PaymentDetails /> },
+  { path: "/add-vehicle", element: <AddVehicle /> },
 ]);
 
 export default router;
