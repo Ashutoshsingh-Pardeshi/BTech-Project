438927f56ab587b1f8552fa9c6c55b7bac748be2
Ashutoshsingh Pardeshi <ashutoshsingh.pardeshi@pattern.com>
2023-07-13 09:51:25 +0530

    feat:added new route and comments

--- backend/routes/ParkingDetails.routes.js
+++ backend/routes/ParkingDetails.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const ParkingDetails = require("../models/ParkingDetails.model");
 const router = express.Router();
 
+/**
+ * @route GET /api/parkings
+ * @desc Gets all the parking spots
+ */
 router.get("", (req, res) => {
   ParkingDetails.find({}).then((spots) => {
     res.status(201);
@@ -9,6 +13,10 @@ router.get("", (req, res) => {
   });
 });
 
+/**
+ * @route GET /api/parkings/availableSpot
+ * @desc Get a single free parking spot
+ */
 router.get("/availableSpot", (req, res) => {
   ParkingDetails.findOne({ isOccupied: false }, "parkingSpot").then((spots) => {
     res.status(200);
@@ -16,6 +24,10 @@ router.get("/availableSpot", (req, res) => {
   });
 });
 
+/**
+ * @route GET /api/parkings/parkedSpots
+ * @desc Gets only the occupied parking spots
+ */
 router.get("/parkedSpots", (req, res) => {
   ParkingDetails.find({ isOccupied: true }).then((spots) => {
     res.status(200);
@@ -23,6 +35,10 @@ router.get("/parkedSpots", (req, res) => {
   });
 });
 
+/**
+ * @route GET /api/parkings/:ownerId
+ * @desc Get the parking spot occupied by specific owner
+ */
 router.get("/:ownerId", (req, res) => {
   const { ownerId } = req.params;
 
@@ -32,6 +48,10 @@ router.get("/:ownerId", (req, res) => {
   });
 });
 
+/**
+ * @route POST /api/parkings/addSpot/:parkingSpot
+ * @desc Create a new parking spot
+ */
 router.post("/addSpot/:parkingSpot", (req, res) => {
   const { parkingSpot } = req.params;
   try {
@@ -51,6 +71,10 @@ router.post("/addSpot/:parkingSpot", (req, res) => {
   console.log(ownerId, parkedSpot);
 });
 
+/**
+ * @route PUT /api/parkings/occupy/:ownerId/:parkingSpot
+ * @desc Allocate a parking spot to an owner
+ */
 router.put("/occupy/:ownerId/:parkingSpot", (req, res) => {
   const { ownerId, parkingSpot } = req.params;
 
@@ -63,11 +87,31 @@ router.put("/occupy/:ownerId/:parkingSpot", (req, res) => {
   });
 });
 
+/**
+ * @route PUT /api/parkings/vacate/:parkingSpot
+ * @desc Remove the user from a parking spot using parkingSpot
+ */
 router.put("/vacate/:parkingSpot", (req, res) => {
   const { parkingSpot } = req.params;
 
   ParkingDetails.findOneAndUpdate(
     { parkingSpot },
+    { isOccupied: false, ownerId: null, checkIn: null }
+  ).then((spot) => {
+    res.status(200);
+    res.send(spot);
+  });
+});
+
+/**
+ * @route PUT /api/parkings/vacate/:ownerId
+ * @desc Remove the user from a parking spot using ownerId
+ */
+router.delete("/checkOut/:ownerId", (req, res) => {
+  const { ownerId } = req.params;
+
+  ParkingDetails.findOneAndUpdate(
+    { ownerId },
     { isOccupied: false, ownerId: null, checkIn: null }
   ).then((spot) => {
     res.status(200);
